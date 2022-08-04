687aa725e344603cce1d7ca41e9ea7aaf85e5572
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-04 05:17:52 +0500

    package file changed

---
+++ kinder-front/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  i18n:{
+    locales:["de","en"],
+    defaultLocale: "de",
+    // localeDetection: true,
+  },
+  reactStrictMode: true ,
+  swcMinify: true,
+}
