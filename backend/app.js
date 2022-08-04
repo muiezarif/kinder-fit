567c99240fb55aa6ccd025bbdad8ac3fc285a64d
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-04 17:00:04 +0500

    backend updated

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import contactUsRouter from "./routes/contactRoute"
 import productRouter from "./routes/productRoute"
 import adminBroRouter from "./routes/adminRoute"
+import * as path from "path";
 
 const app = express()
 app.use(cors())
@@ -10,9 +11,10 @@ app.use(express.json())
 app.use("/api/kinderfit",contactUsRouter)
 app.use("/api/kinderfit",productRouter)
 app.use("/admin", adminBroRouter);
+const dir = __dirname;
 const sendFile = (file) => (req, res) => res.sendFile(path.resolve(dir, 'build', file));
 const pathMap = new Set([
-    '/settings', '/home' // All available static paths
+    '/', '/home' // All available static paths
 ]);
 app.use(express.static(path.join(dir, 'build')));
 app.get('*', (req, res) => {
