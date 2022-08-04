62e06f049fc99d06a12f19ea5e434ea6bc4fe15f
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-04 16:54:06 +0500

    backend app.js changed

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,15 @@ app.use(express.json())
 app.use("/api/kinderfit",contactUsRouter)
 app.use("/api/kinderfit",productRouter)
 app.use("/admin", adminBroRouter);
-
+const sendFile = (file) => (req, res) => res.sendFile(path.resolve(dir, 'build', file));
+const pathMap = new Set([
+    '/settings', '/home' // All available static paths
+]);
+app.use(express.static(path.join(dir, 'build')));
+app.get('*', (req, res) => {
+    const p = req.path;
+    if (pathMap.has(p)) res.sendFile(path.resolve(dir, 'build', `${p.slice(1)}.html`));
+    else res.sendFile(path.resolve(dir, 'build', '404.html'));
+});
 
 export default app;
