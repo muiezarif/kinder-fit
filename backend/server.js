b7653b67ccfe6f3006fb7abfda08d982f93048d2
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-05 18:30:34 +0500

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,19 @@ app.listen(process.env.PORT,() => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
 if (process.env.NODE_ENV === "production"){
-    app.use(express.static('kinder-front/build'))
+    app.use(express.static('../kinder-front/build'))
     app.get('*', (req, res) => {
 
     });
+// const dir = __dirname;
+// const sendFile = (file) => (req, res) => res.sendFile(path.resolve(dir, 'build', file));
+// const pathMap = new Set([
+//     '/', '/home' // All available static paths
+// ]);
+// app.use(express.static(path.join(dir, 'build')));
+// app.get('*', (req, res) => {
+//     const p = req.path;
+//     if (pathMap.has(p)) res.sendFile(path.resolve(dir, 'build', `${p.slice(1)}.html`));
+//     else res.sendFile(path.resolve(dir, 'build', '404.html'));
+// });
 }
