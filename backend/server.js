02214dd9e4d1baaaffde3fd15e57a4daa37a6cba
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-05 17:38:52 +0500

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import app from "./app";
 import dotenv from "dotenv"
 import connectDatabase from "./db/database";
+import path from "path";
+import express from "express";
 
 // Config 
 dotenv.config({path:"config/config.env"})
@@ -9,3 +11,9 @@ connectDatabase()
 app.listen(process.env.PORT,() => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
 })
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static('kinder-front/build'))
+    app.get('*', (req, res) => {
+
+    });
+}
