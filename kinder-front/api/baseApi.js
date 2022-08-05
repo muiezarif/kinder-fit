c44cff4613406f5a894c6689fca9294e19fefc71
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-05 20:03:16 +0500

    backend updated

--- kinder-front/api/baseApi.js
+++ kinder-front/api/baseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"http://localhost:8080/api/kinderfit"
+    baseURL:"http://134.122.22.161/api/kinderfit"
     // baseURL:"https://digitvl.live/api/v1"
     // baseURL:"https://96db-119-63-138-150.ngrok.io/api/v1"
 });
