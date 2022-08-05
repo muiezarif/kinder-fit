06a274d1cb00c555af35b8c4a28cd03f8a236a49
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-05 22:13:08 +0500

    frontend updated

--- kinder-front/next.config.js
+++ kinder-front/next.config.js
@@ -8,27 +8,3 @@ module.exports = {
   },
   reactStrictMode: true
 }
-module.exports = {
-  exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/home': { page: '/home' }
-    }
-  },
-}
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
-};
