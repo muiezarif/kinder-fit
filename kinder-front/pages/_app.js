54fd3a92dbd9749c376338b493638f7c2afd9a1e
muiezarif <36100308+muiezarif@users.noreply.github.com>
2022-08-06 19:52:47 +0500

    frontend updated

--- kinder-front/pages/_app.js
+++ kinder-front/pages/_app.js
@@ -9,7 +9,12 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 import reducers from '../redux/reducers'
 import '../styles/globals.css'
-import '../public/css/style.css'
+import '../public/vendor/aos/aos.css'
+import '../public/vendor/bootstrap/css/bootstrap.min.css'
+import '../public/vendor/bootstrap-icons/bootstrap-icons.css'
+import '../public/vendor/boxicons/css/boxicons.min.css'
+import '../public/vendor/glightbox/css/glightbox.min.css'
+import '../public/vendor/swiper/swiper-bundle.min.css'
 let store
 
 
